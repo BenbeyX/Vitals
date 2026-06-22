1258d3a0c2cf2d6e93c5d8730152c533b15a5960
Benbey <44267199+BenbeyX@users.noreply.github.com>
2026-06-22 11:20:42 +0100

    Initial project structure setup
    Also some launch settings for my IDE

---
+++ src/test/java/io/vitals/game/VitalsApplicationTests.java
@@ -0,0 +1,12 @@
+package io.vitals.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
