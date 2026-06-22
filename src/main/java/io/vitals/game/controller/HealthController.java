acd8d97402b230c3ef328d1d804724a734edb2f4
Benbey <44267199+BenbeyX@users.noreply.github.com>
2026-06-22 11:26:40 +0100

    Create HealthController.java
    Added my first API call

---
+++ src/main/java/io/vitals/game/controller/HealthController.java
@@ -0,0 +1,16 @@
+package io.vitals.game.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "{status: 'OK'}";
+    }
+    
+}
