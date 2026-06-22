ef8ccc1f8a65b3d9947926a31bc421a5a8d2cdf2
Benbey <44267199+BenbeyX@users.noreply.github.com>
2026-06-22 12:08:49 +0100

    Error in position tests

--- src/test/java/io/vitals/game/model/PositionTest.java
+++ src/test/java/io/vitals/game/model/PositionTest.java
@@ -8,21 +8,21 @@ public class PositionTest {
     @Test
     public void testPosition() {
         Position position = new Position(10, 20);
-        assertEquals(10, position.getX());
-        assertEquals(20, position.getY());
+        assertEquals(position.getX(), 10, "X should be 10");
+        assertEquals(position.getY(), 20, "Y should be 20");
     }
 
     @Test
     public void testPositionEquals() {
         Position position1 = new Position(10, 20);
         Position position2 = new Position(10, 20);
-        assertEquals(position1, position2);
+        assertEquals(position1.equals(position2), true, "Positions should be equal");
     }
 
     @Test
     public void testPositionNotEquals() {
         Position position1 = new Position(10, 20);
         Position position2 = new Position(10, 30);
-        assertNotEquals(position1, position2);
+        assertNotEquals(position1.equals(position2), true, "Positions should not be equal");
     }
 }
