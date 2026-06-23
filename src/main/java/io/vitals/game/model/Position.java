41daca93569e9b1306c05689ca249054164ae5eb
Benbey <44267199+BenbeyX@users.noreply.github.com>
2026-06-23 09:42:47 +0100

    had to override position equals

--- src/main/java/io/vitals/game/model/Position.java
+++ src/main/java/io/vitals/game/model/Position.java
@@ -1,4 +1,5 @@
 package io.vitals.game.model;
+import java.util.Objects;
 
 public class Position {
     private double x;
@@ -7,6 +8,20 @@ public class Position {
     public Position(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Position position = (Position) obj;
+        return Double.compare(position.x, x) == 0 && 
+           Double.compare(position.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+    return Objects.hash(x, y);
     }
     
     public double getX() {
