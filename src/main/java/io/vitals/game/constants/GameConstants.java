a50ac05e33ddecb18e318c23536545c32f80f256
Benbey <44267199+BenbeyX@users.noreply.github.com>
2026-06-23 10:27:59 +0100

    Create GameConstants.java

---
+++ src/main/java/io/vitals/game/constants/GameConstants.java
@@ -0,0 +1,22 @@
+package io.vitals.game.constants;
+
+public class GameConstants {
+    public static final int MOVEMENT_SPEED = 200;
+    public static final int DASH_SPEED = 400;
+    public static final int MAX_DASH_DISTANCE = 300;
+    public static final int Q_ATTACK_RANGE = 150;
+    public static final int RIGHT_CLICK_ATTACK_RANGE = 20;
+    public static final int ARENA_SIZE_X = 800;
+    public static final int ARENA_SIZE_Y = 600;
+    public static final int PLAYER_RADIUS = 15;
+    public static final int OPPONENT_RADIUS = 20;
+    public static final int VITAL_CONE_ANGLE = 90;
+    public static final int VITAL_SWITCH_DELAY = 1;
+    public static final int SPEED_BOOST = 20;
+    public static final int DODGE_COOLDOWN = 3;
+    public static final int PROXIMITY_TRIGGER_RANGE = 300;
+    public static final int DEFAULT_HP = 5;
+    public static final int DEFAULT_Q_COOLDOWN = 5;
+    public static final double RIGHT_CLICK_ATTACK_COOLDOWN = 0.5;
+    public static final int RESPAWN_DELAY = 1;
+}
